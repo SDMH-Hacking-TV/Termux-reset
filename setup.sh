9c54d7bc21c9f758637a2e72cc74408c9dc54404
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-21 13:38:05 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,6 @@ sed 's+SDMH+'$user'+g' etc/bash.bashrc.dpkg-dist > /data/data/com.termux/files/u
 sed 's+SDMH+'$user'+g' etc/bindresvport.blacklist > /data/data/com.termux/files/usr/etc/bindresvport.blacklist
 sed 's+SDMH+'$user'+g' etc/inputrc > /data/data/com.termux/files/usr/etc/inputrc
 sed 's+SDMH+'$user'+g' etc/motd > /data/data/com.termux/files/usr/etc/motd
-echo -e  $g "Please  Wait  A few Minutes...... "
 sed 's+SDMH+'$user'+g' etc/motd.dpkg-dist > /data/data/com.termux/files/usr/etc/motd.dpkg-dist
 sed 's+SDMH+'$user'+g' etc/nanorc > /data/data/com.termux/files/usr/etc/nanorc
 sed 's+SDMH+'$user'+g' etc/netconfig > /data/data/com.termux/files/usr/etc/netconfig
@@ -30,6 +29,7 @@ sed 's+SDMH+'$user'+g' etc/tls > /data/data/com.termux/files/usr/etc/tls
 rm -rf etc
 rm -fr Termux-reset
 
+echo -e  $g "Welcome to new EnterFace "
 echo ""
 printf "                   \e[1;92m [!] Please restart termux!\e[0m\n"
 

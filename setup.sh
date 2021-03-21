e3e004ece88198c11e296fa49117e3f2651ac0f7
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-21 13:36:30 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,14 +8,18 @@ echo ""
 apt update 
 apt upgrade -y 
 pkg install python
+echo -e  $g "Please  Wait  A few Minutes...... "
 pkg install python2
 Pkg install nano
+echo -e  $g "Please  Wait  A few Minutes...... "
 git clone https://github.com/Hayper-Power/etc
+echo -e  $g "Please  Wait  A few Minutes...... "
 sed 's+SDMH+'$user'+g' etc/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
 sed 's+SDMH+'$user'+g' etc/bash.bashrc.dpkg-dist > /data/data/com.termux/files/usr/etc/bash.bashrc.dpkg-dist
 sed 's+SDMH+'$user'+g' etc/bindresvport.blacklist > /data/data/com.termux/files/usr/etc/bindresvport.blacklist
 sed 's+SDMH+'$user'+g' etc/inputrc > /data/data/com.termux/files/usr/etc/inputrc
 sed 's+SDMH+'$user'+g' etc/motd > /data/data/com.termux/files/usr/etc/motd
+echo -e  $g "Please  Wait  A few Minutes...... "
 sed 's+SDMH+'$user'+g' etc/motd.dpkg-dist > /data/data/com.termux/files/usr/etc/motd.dpkg-dist
 sed 's+SDMH+'$user'+g' etc/nanorc > /data/data/com.termux/files/usr/etc/nanorc
 sed 's+SDMH+'$user'+g' etc/netconfig > /data/data/com.termux/files/usr/etc/netconfig
@@ -26,10 +30,10 @@ sed 's+SDMH+'$user'+g' etc/tls > /data/data/com.termux/files/usr/etc/tls
 rm -rf etc
 rm -fr Termux-reset
 
-echo "Please restart termux!"
-printf "                \e[1;92m [!] Please restart termux!\e[0m\n"
-	sleep 1
-	exit
+echo ""
+printf "                   \e[1;92m [!] Please restart termux!\e[0m\n"
+
+exit
 
 
 

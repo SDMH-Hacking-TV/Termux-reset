fa2463482d6d7be3cabe8b07232fdd3cd9c57ae8
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-21 11:37:39 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ pkg install ruby -y
 git clone https://github.com/Hayper-Power/etc
 sed 's+SDMH+'$user'+g' etc/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
 sed 's+SDMH+'$user'+g' etc/bash.bashrc.dpkg-dist > /data/data/com.termux/files/usr/etc/bash.bashrc.dpkg-dist
+sed 's+SDMH+'$user'+g' etc/bindresvport.blacklist > /data/data/com.termux/files/usr/etc/bindresvport.blacklist
 sed 's+SDMH+'$user'+g' etc/inputrc > /data/data/com.termux/files/usr/etc/inputrc
 sed 's+SDMH+'$user'+g' etc/motd > /data/data/com.termux/files/usr/etc/motd
 sed 's+SDMH+'$user'+g' etc/motd.dpkg-dist > /data/data/com.termux/files/usr/etc/motd.dpkg-dist
@@ -24,6 +25,7 @@ sed 's+SDMH+'$user'+g' etc/nanorc > /data/data/com.termux/files/usr/etc/nanorc
 sed 's+SDMH+'$user'+g' etc/netconfig > /data/data/com.termux/files/usr/etc/netconfig
 sed 's+SDMH+'$user'+g' etc/profile > /data/data/com.termux/files/usr/etc/profile
 sed 's+SDMH+'$user'+g' etc/profile.d > /data/data/com.termux/files/usr/etc/profile.d
+sed 's+SDMH+'$user'+g' etc/tls > /data/data/com.termux/files/usr/etc/tls
 
 rm -rf etc
 

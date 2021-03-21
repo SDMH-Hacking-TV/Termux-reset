b94201f1f6024d586ac397e988208e8df2da7df6
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-21 11:12:41 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,18 @@ pkg install python2
 pip install pyfiglet
 pip install termcolor
 pkg install ncurses-utils -y 
+Pkg install nano
 pkg install ruby -y 
-git clone https://github.com/BDhackers009/simple-edit
-sed 's+THBD+'$user'+g' simple-edit/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
-sed 's+THBD+'$user'+g' simple-edit/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
-rm -rf simple-edit
+git clone https://github.com/Hyper-power/etc
+sed 's+SDMH+'$user'+g' etc/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+SDMH+'$user'+g' etc/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+sed 's+SDMH+'$user'+g' etc/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+SDMH+'$user'+g' etc/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+sed 's+SDMH+'$user'+g' etc/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+SDMH+'$user'+g' etc/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+sed 's+SDMH+'$user'+g' etc/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+SDMH+'$user'+g' etc/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+sed 's+SDMH+'$user'+g' etc/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+
+rm -rf etc
 
